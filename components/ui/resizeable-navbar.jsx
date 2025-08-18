d51b5924dc5de16ecdf1aa518fe10458e02519aa
unknown <pikinleon39@gmail.com>
2025-08-18 22:01:28 +0100

    add a dark property to mobile nav button

--- components/ui/resizeable-navbar.jsx
+++ components/ui/resizeable-navbar.jsx
@@ -213,7 +213,7 @@ export const NavbarButton = ({
   ...props
 }) => {
   const baseStyles =
-    "px-4 py-2 rounded-md bg-white button bg-white text-black text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center";
+    "px-4 py-2 rounded-md bg-white button bg-black text-white dark:bg-white dark:text-black  text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center";
 
   const variantStyles = {
     primary:
